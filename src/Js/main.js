33ed1ea772f3f606b3118b8f2e784c7bc2abffed
Beleir <117771672+beleir101@users.noreply.github.com>
2023-12-17 15:27:40 +0300

    Update main.js

--- src/Js/main.js
+++ src/Js/main.js
@@ -2,8 +2,8 @@ const openMenu = document.querySelector(".open-menu");
 
 const closeMenu = document.querySelector(".close-menu");
 const offcanvasMenu = document.querySelector(".offcanvas-menu");
+const sub = document.getElementById("submitted");
 
-console.log(openMenu, closeMenu, offcanvasMenu);
 openMenu.addEventListener("click", function (e) {
   e.preventDefault();
   offcanvasMenu.classList.add("active");
@@ -11,4 +11,8 @@ openMenu.addEventListener("click", function (e) {
 closeMenu.addEventListener("click", function (e) {
   e.preventDefault();
   offcanvasMenu.classList.remove("active");
+});
+sub.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert("Message Sent Succesfully.");
 });
